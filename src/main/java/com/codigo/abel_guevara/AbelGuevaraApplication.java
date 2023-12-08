79e3203745adb2f6a5f187974563e9f440a9774b
AbelG9 <55357845+AbelG9@users.noreply.github.com>
2023-12-07 23:25:59 -0500

    Initial commit

---
+++ src/main/java/com/codigo/abel_guevara/AbelGuevaraApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.abel_guevara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbelGuevaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbelGuevaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codigo/abel_guevara/AbelGuevaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.abel_guevara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbelGuevaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
