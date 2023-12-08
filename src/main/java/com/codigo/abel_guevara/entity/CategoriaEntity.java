74028607a05d4f250e0fbfc495671f83357e1841
AbelG9 <55357845+AbelG9@users.noreply.github.com>
2023-12-07 23:29:39 -0500

    entities created

---
+++ src/main/java/com/codigo/abel_guevara/entity/CategoriaEntity.java
@@ -0,0 +1,27 @@
+package com.codigo.abel_guevara.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "categoria")
+public class CategoriaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private Integer estado;
+
+    @ManyToMany(mappedBy = "categorias")
+    @JsonIgnore
+    private Set<LibroEntity> libroEntitySet = new HashSet<>();
+}
---
+++ src/main/java/com/codigo/abel_guevara/entity/LibroEntity.java
@@ -0,0 +1,42 @@
+package com.codigo.abel_guevara.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "libro")
+public class LibroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    @OneToOne
+    @JoinColumn(name = "editor_id")
+    private EditorEntity editor;
+
+    private Integer estado;
+
+    @ManyToMany
+    @JoinTable(
+            name = "libro_categoria",
+            joinColumns = @JoinColumn(name = "libro_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private Set<CategoriaEntity> categorias = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "libro_autor",
+            joinColumns = @JoinColumn(name = "libro_id"),
+            inverseJoinColumns = @JoinColumn(name = "autor_id")
+    )
+    private Set<AutorEntity> autores = new HashSet<>();
+}
